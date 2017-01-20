a4f1df60ebfdab7b470cf8b78f5fa8f9cb891498
don <sleepyparadox@gmail.com>
2017-01-20 22:30:19 +1100

    twitch stream working thanks Grahnz! using https://github.com/Grahnz/TwitchIRC-Unity

---
+++ TwitchPoles/Assets/Scripts/Game.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class Game : MonoBehaviour
+{
+    public TextMesh Text;
+    TwitchIRC _irc;
+    MessageDebugger _messageDebugger;
+
+    void Start ()
+    {
+        _irc = this.GetComponent<TwitchIRC>();
+
+        _messageDebugger = new MessageDebugger(Text);
+
+        if (_irc.enabled)
+            _irc.messageRecievedEvent.AddListener(MessageRecieved);
+        else
+            StartCoroutine(DoFakeUpdate());
+
+    }
+
+    IEnumerator DoFakeUpdate()
+    {
+        var lines = new List<string>();
+        foreach(var file in Directory.GetFiles(Directory.GetCurrentDirectory()+"/Assets/Resources/TwitchLogs"))
+            lines.AddRange(File.ReadAllLines(file));
+
+        var i = 0;
+        while (true)
+        {
+            var line = lines[i];
+            i = (i + 1) % lines.Count;
+
+            MessageRecieved(line);
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0.1f, 1f));
+        }
+    }
+
+
+
+    private void MessageRecieved(string msg)
+    {
+        var logPath = string.Concat(Directory.GetCurrentDirectory(), "/Assets/Resources/TwitchLogs/", _irc.channelName, ".txt");
+        using (var writer = new StreamWriter(logPath, true))
+            writer.WriteLine(msg);
+
+        var pipeAt = msg.IndexOf(':', 2);
+        if (pipeAt >= 0)
+        {
+            pipeAt++;
+            msg = msg.Substring(pipeAt, msg.Length - pipeAt);
+        }
+
+        _messageDebugger.MessageRecieved(msg);
+    }
+}
---
+++ TwitchPoles/Assets/Scripts/MessageDebugger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class MessageDebugger
+    {
+        const int MaxMessages = 50;
+        List<string> _msgs;
+        TextMesh _text;
+        public MessageDebugger(TextMesh text)
+        {
+            _text = text;
+            _msgs = new List<string>();
+        }
+
+        public void MessageRecieved(string msg)
+        {
+            _msgs.Add(msg);
+            if(_msgs.Count > MaxMessages)
+                _msgs.RemoveAt(0);
+
+            _text.text = string.Join("\n", _msgs.ToArray());
+        }
+    }
+}
